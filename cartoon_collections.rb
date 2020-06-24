1e5a877bdb984f10cb713adc568c7d700af616c9
Cameron Hudson <camnn3@yahoo.com>
2020-06-24 17:22:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,9 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(calls)
-  calls.map do |name|
-    name.capitalize << "!"
+    calls.map do |name|
+      name.capitalize << "!"
+  end
 end
 
 def long_planeteer_calls(planteer_calls)
